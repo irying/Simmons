0d1b5c36be0be55cf848628458e214606a7d90a5
irying <kyrieup@gmail.com>
2017-12-27 21:51:11 +0800

    需要个Acceptor开始，需要个循环进行，需要个map维护连接

--- mini-muduo/TcpServer.h
+++ mini-muduo/TcpServer.h
@@ -1,5 +1,27 @@
 #ifndef TCPSERVER_H
 #define TCPSERVER_H
 
-#include 
+#include <sys/epoll.h>
+#include "Declear.h"
+#include "Define.h"
+#include "IAcceptorCallback.h"
+#include <map>
+
+using namespace std;
+
+class TcpServer : public IAcceptorCallback
+{
+public:
+    TcpServer(EventLoop* loop);
+    ~TcpServer();
+    void start();
+    virtual void newConnection(int sockfd);
+private:
+    struct epoll_event _events[MAX_EVENTS];
+    map<int, TcpConnection*> _connections;
+    Acceptor* _pAcceptor;
+    EventLoop* _loop;
+};
+
+
 #endif
