61d025054c55feddb9fc7fed6bd788bb3b3bfe81
irying <kyrieup@gmail.com>
2017-12-27 21:30:51 +0800

    init

---
+++ mini-muduo/Declear.h
@@ -0,0 +1,12 @@
+#ifndef DECLEAR_H
+#define DECLEAR_H
+
+class IChannelCallback;
+class IAcceptorCallback;
+class Channel;
+class Acceptor;
+class TcpConnection;
+class EventLoop;
+class Epoll;
+
+#endif
---
+++ mini-muduo/TcpServer.h
@@ -0,0 +1,5 @@
+#ifndef TCPSERVER_H
+#define TCPSERVER_H
+
+#include 
+#endif
